0272a4e3515fc3ebf46ab3c1d5c4aea06645b619
renan-nobrega <renan.nobrega@outlook.com>
2020-01-30 15:47:43 -0300

    limpando codigo

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+
+<!-- ARQUIVO HEAD COM OS CSS -->
+<?php require_once "layout/head.php"?>
+
+</head>
+<body>
+	<!-- Page Preloder -->
+	<div id="preloder">
+		<div class="loader"></div>
+	</div>
+
+    <!-- MENU -->
+    <?php require_once "layout/menu.php"?>
+
+
+    <!-- conteúdo principal -->
+    <?php require_once "content/home.php"?>
+
+    <!-- Footer section -->
+    <?php require_once "layout/footer.php"?>
+	
+	</body>
+</html>
